811861565586d4943f5972c06d06b895da70d5c4
redbird <redbirda47@gmail.com>
2018-05-20 15:58:25 +0300

    Initial Observer commit. Also deleted some binary files.

---
+++ Letalka/ISubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letalka
+{
+    interface ISubject
+    {
+        void AddObserver(IObserver observer);
+        void DeleteObserver(IObserver observer);
+        void Notify();
+    }
+}
